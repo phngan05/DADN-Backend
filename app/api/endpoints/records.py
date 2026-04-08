3856dfdedd08ec226ca068af2556de51f1aad332
phngan05 <phngan100705@gmail.com>
2026-04-08 15:22:10 +0700

    update mqtt connection to Adafruit

--- app/api/endpoints/records.py
+++ app/api/endpoints/records.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter, HTTPException, Depends
 from app.core.adafruit import active_adafruit_sessions
 from app.core.security import get_current_user_id
 from app.schemas.record import RecordUpdate
+from app.core.adafruit import AdafruitMQTT, active_adafruit_sessions
+from app.core.database import supabase_client
+import asyncio
+
 router = APIRouter()
 
 def get_user_mqtt(user_id: str = Depends(get_current_user_id)):
@@ -9,9 +13,25 @@ def get_user_mqtt(user_id: str = Depends(get_current_user_id)):
         raise HTTPException(status_code=404, detail="Adafruit session not active. Please login again.")
     return active_adafruit_sessions[user_id]
 
+async def get_or_create_mqtt_service(user_id: str):
+    if user_id not in active_adafruit_sessions:
+        ada_res = supabase_client.table("ADAFRUIT_SERVER").select("*").eq("user_id", user_id).execute()
+        if not ada_res.data:
+            raise HTTPException(status_code=404, detail="Adafruit config not found")
+        
+        config = ada_res.data[0]
+        loop = asyncio.get_event_loop()
+        new_service = AdafruitMQTT(config["username"], str(user_id), config["api_key"], loop=loop)
+        new_service.start()
+        active_adafruit_sessions[str(user_id)] = new_service
+        await asyncio.sleep(1) 
+        
+    return active_adafruit_sessions[str(user_id)]
+
 @router.get("/all")
-def get_all_feeds(mqtt_service = Depends(get_user_mqtt)):
-    """Get all feed data for the user"""
+async def get_all_feeds(user_id: str = Depends(get_current_user_id)):
+    """API này vừa kích hoạt kết nối, vừa lấy dữ liệu hiện tại"""
+    mqtt_service = await get_or_create_mqtt_service(user_id)
     return mqtt_service.feeds_data
 
 @router.get("/history/{feed_key}")
--- app/core/security.py
+++ app/core/security.py
@@ -27,7 +27,7 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def create_access_token(data: dict, expires_delta: timedelta = None):
     """Create JWT Token"""
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=15))
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=60*24*7))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.DATABASE_JWT_SECRET, algorithm=ALGORITHM)
 
