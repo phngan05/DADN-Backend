adb7948655661d5f6c99d8725011a980cd69910d
phngan05 <phngan100705@gmail.com>
2026-05-03 11:04:30 +0700

    fix path to model

--- app/models/models.py
+++ app/models/models.py
@@ -2,6 +2,6 @@ import os
 import joblib
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(BASE_DIR, "models", "fan_model.pkl")
+model_path = os.path.join(BASE_DIR, "fan_model.pkl")
 
 MODEL = joblib.load(model_path)
